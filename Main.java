dee929855acd5341a5a9c65aadd301c4a8f8dc6b
sara <sara.alshayeb59@gmail.com>
2023-09-06 23:18:58 +0300

    selection sort

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void selectionSort(int[] arr) {
         int n = arr.length;
+        int m=0;
 
         for (int i = 0; i < n - 1; i++) {
             int minIndex = i;
